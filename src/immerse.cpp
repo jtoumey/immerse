fef1a20b0c00915811d07705532f350071d52c84
Julian Toumey <jtoumey@gmail.com>
2018-06-02 22:32:51 -0500

    Reworked data structure to prepare quadTree class. Also tried to modularize other structs/objects that will be used frequently.

--- src/immerse.cpp
+++ src/immerse.cpp
@@ -1,6 +1,106 @@
 
 #include <iostream>
 
+struct point
+{
+    // generic point, for reuse
+    float x;
+    float y;
+
+    // constructor
+    point(float _x, float _y) { x = _x; y = _y; }
+    // additional default constructor
+    point () { x = 0.0; y = 0.0; }
+};
+
+struct treeNode
+{
+    // a subdivision of the quadtree
+
+    // node location
+    point loc;
+
+    // information regarding intersection at node
+    int nodeData;
+    // constructors
+    treeNode ( point _loc, int _nodeData ) { loc = _loc; nodeData = _nodeData; }
+    // default, no position 
+    treeNode () { nodeData = 0; }
+};
+
+struct boundBox
+{
+    // general bound box for polygon
+    // future implementation
+    //std::vector<float> center;
+    // or just make this a struct point
+    float centerX;
+    float centerY;
+
+    float bBoxDx;
+    float bBoxDy;
+    boundBox ( float _centerX, float _centerY, float _bBoxDx, float _bBoxDy ) 
+    {
+        centerX = _centerX;
+        centerY = _centerY;
+        bBoxDx = _bBoxDx;
+        bBoxDy = _bBoxDy;
+    }
+    boundBox () { centerX = 0.0; centerY = 0.0; bBoxDx = 0.0; bBoxDy = 0.0; } 
+};
+
+struct nGon
+{
+    // the input polygon to immerse
+    int numEdges;
+    std::vector<int> edgeList();
+    std::vector<float> vertX(), vertY();
+    // hide to debug
+   // boundBox nGonBbox;
+};
+
+class quadTree {
+
+    // for the current node in the tree, reuse the boundBox to store node geometry
+    boundBox nodeBound;
+
+    // for the current node, store location and intersection data
+    treeNode *n;
+
+    // child nodes
+    quadTree *nw;
+    quadTree *ne;
+    quadTree *se;
+    quadTree *sw;
+
+    public:
+        // default constructor
+        quadTree ()
+        {
+            nodeBound = boundBox(0, 0, 0, 0);
+            n = NULL;
+            nw = NULL;
+            ne = NULL;
+            se = NULL;
+            sw = NULL;
+        }
+        // constructor
+        quadTree (boundBox _bound)
+        {
+            nodeBound = boundBox(_bound);
+            n = NULL;
+            nw = NULL;
+            ne = NULL;
+            se = NULL;
+            sw = NULL;
+        }
+    void insert(treeNode*);
+    treeNode* search(point);
+    bool inBoundary(point);
+
+
+};
+
 float findMin (int dim, float inpArray[]) {
     float min = inpArray[0];
     for( int n = 1; n < dim; n++) {
@@ -25,7 +125,9 @@ int main () {
 
     // trivial two-dimensional n-gon
 
-    int nGon = 4;
+    nGon polygon0; 
+    /*
+//    int nGon = 4;
     float vertX[] = {2.0, 1.0, 4.0, 5.0};
     float vertY[] = {1.0, 3.0, 4.0, 2.0};
     float bBoxDx, bBoxDy;
@@ -55,4 +157,5 @@ int main () {
     for ( int n = 0; n < nGon; n++ ) {
         std::cout << "(x, y) = (" << bBoxX[n] << ", " << bBoxY[n] << ")\n";
     }
+    */
 }
